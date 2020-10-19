038467f6822dd8f03dcaf28a1c25846bfbcd62cb
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-19 16:15:08 +0200

    use of build in file system object

--- logger.js
+++ logger.js
@@ -3,16 +3,25 @@ const patchObj = path.parse(__filename);
 
 const os = require('os');
 
-const totalMemory = os.totalmem();
-const freeMemory = os.freemem();
+// const totalMemory = os.totalmem();
+// const freeMemory = os.freemem();
+//
+// function log(message) {
+// //    send an http request
+//     console.log('log from logger', message);
+// }
+// // console.log(module);
+// // console.log(patchObj);
+// console.log('totalmem',totalMemory);
+// console.log('freemem',freeMemory);
+// module.exports = log;
 
-function log(message) {
-//    send an http request
-    console.log('log from logger', message);
-}
-// console.log(module);
-// console.log(patchObj);
-console.log('totalmem',totalMemory);
-console.log('freemem',freeMemory);
-module.exports = log;
+const fs = require('fs');
 
+// const files = fs.readdirSync('./');
+// console.log(files);
+
+fs.readdir('./', (err, files)=>{
+    if (err) console.log('error',err);
+    else console.log('result' ,files);
+});
