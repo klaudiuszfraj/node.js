4a08916f35d04127fbebe7db9e9879884d67df82
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-19 15:57:38 +0200

    use of build in path object

--- logger.js
+++ logger.js
@@ -1,9 +1,12 @@
-const URL = 'http://mylogger.io/log';
+const path = require('path');
+const patchObj = path.parse(__filename);
+
 
 function log(message) {
 //    send an http request
     console.log('log from logger', message);
 }
 console.log(module);
+console.log(patchObj);
 module.exports = log;
 
