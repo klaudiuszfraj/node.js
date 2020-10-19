3be3f55153a21ce35903da5cb15991c8dbfc284f
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-19 16:25:56 +0200

    create an event

--- logger.js
+++ logger.js
@@ -1,27 +1,12 @@
-const path = require('path');
-const patchObj = path.parse(__filename);
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
-const os = require('os');
+//Register a listener
+emitter.on('messageLogged', ()=>{
+    console.log('listener called');
+})
 
-// const totalMemory = os.totalmem();
-// const freeMemory = os.freemem();
-//
-// function log(message) {
-// //    send an http request
-//     console.log('log from logger', message);
-// }
-// // console.log(module);
-// // console.log(patchObj);
-// console.log('totalmem',totalMemory);
-// console.log('freemem',freeMemory);
-// module.exports = log;
 
-const fs = require('fs');
 
-// const files = fs.readdirSync('./');
-// console.log(files);
-
-fs.readdir('./', (err, files)=>{
-    if (err) console.log('error',err);
-    else console.log('result' ,files);
-});
+//Raise an event
+emitter.emit('messageLogged')
