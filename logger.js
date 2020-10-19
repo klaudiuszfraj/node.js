8ad31c08734155744132c8d657b51e8c6b4f82e3
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-19 18:05:00 +0200

    create an event with argument

--- logger.js
+++ logger.js
@@ -2,11 +2,11 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 //Register a listener
-emitter.on('messageLogged', ()=>{
-    console.log('listener called');
+emitter.on('messageLogged', (arg )=>{
+    console.log('listener called', arg);
 })
 
 
 
 //Raise an event
-emitter.emit('messageLogged')
+emitter.emit('messageLogged', {id: 1, message: 'im a argument'})
