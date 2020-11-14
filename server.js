08d27453e6879641bf6a6477ebd137ad0691e779
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-11-14 21:08:13 +0100

    render example articles

--- server.js
+++ server.js
@@ -8,14 +8,20 @@ app.use('/articles', articleRouter);
 
 
 app.get('/', (req, res) => {
-    const articles = {
-        title: 'test title',
-        date: Date.now,
-        description: 'test description'
-    }
+    const articles = [
+        {
+            title: 'test title',
+            date: new Date(),
+            description: 'test description'
+        },
+        {
+            title: 'test title2',
+            date: new Date(),
+            description: 'test description2'
+        },
+    ]
     res.render('index', {articles});
 })
-
 
 
 app.listen(5000);
